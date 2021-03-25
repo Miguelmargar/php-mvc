59ddd392b50aa4c62fd89d9b0a79f5127bf65c7a
miguelmargar <miguelmargar@outlook.com>
2021-03-25 13:47:41 +0000

    added config with constants and spl_autoload_register

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,13 @@
 <?php
-    // Load libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+    // load Config
+    require_once 'config/config.php';
+
+    // Load libraries - not needed if autoload below
+    //require_once 'libraries/core.php';
+    //require_once 'libraries/controller.php';
+    //require_once 'libraries/database.php';
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className) {
+        require_once 'libraries/' . $className . '.php';
+    });
