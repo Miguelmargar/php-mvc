aa63f92798a2fe1832272107e1b69b3fe2453b12
miguelmargar <miguelmargar@outlook.com>
2021-03-26 08:16:30 +0000

    finished login functionality with sessions and logout functionality too

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,27 +1,33 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3" aria-label="Fourth navbar example">
     <div class="container-fluid">
-      <a class="navbar-brand" href="<?= URLROOT; ?>"><?= SITENAME; ?></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <a class="navbar-brand" href="<?= URLROOT; ?>"><?= SITENAME; ?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExample04">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="<?= URLROOT; ?>">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= URLROOT; ?>/pages/about">About</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="<?= URLROOT; ?>/users/register">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= URLROOT; ?>/users/login">Login</a>
-          </li>
-        </ul>
-      </div>
+        <div class="collapse navbar-collapse" id="navbarsExample04">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="<?= URLROOT; ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= URLROOT; ?>/pages/about">About</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto mb-2 mb-md-0">
+                <?php if(isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="<?= URLROOT; ?>/users/register">logOut</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="<?= URLROOT; ?>/users/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URLROOT; ?>/users/login">Login</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
