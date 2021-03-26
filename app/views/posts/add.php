d15b611abb68ba343bde7591cf0f2375e9c6c353
miguelmargar <miguelmargar@outlook.com>
2021-03-26 09:31:11 +0000

    added creating of post functionality to save post data to db

--- app/views/posts/add.php
+++ app/views/posts/add.php
@@ -3,7 +3,7 @@
     <div class="card card-body bg-light mt-5">
         <h2>Add Post</h2>
         <p>Create a post with a form</p>
-        <form action="<?php echo URLROOT; ?>/users/login" method="post">
+        <form action="<?php echo URLROOT; ?>/posts/add" method="post">
               <div class="form-group">
                     <label for="title">Title: <sup>*</sup></label>
                     <input type="text" name="title"
